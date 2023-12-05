d42fd8cc66b46b5ec0fe400f7c67c6bdb790ed90
Wesley <wesmith314@gmail.com>
2023-12-05 11:06:05 -0800

    adds frontend components

---
+++ client/src/components/ArtistCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtistCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ArtistCard
---
+++ client/src/components/ArtworkCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtworkCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ArtworkCard
