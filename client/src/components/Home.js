e906f1532c21a625c5a4736870194f05224cd93e
Wesley <wesmith314@gmail.com>
2023-12-11 14:00:16 -0800

    adds artwork delete to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,55 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from './Helpers/AuthProvider'
 import MenuBar from './MenuBar'
 import Gallery from './Gallery'
+import AlertBar from './Helpers/AlertBar'
 
 function Home() {
-  const { artworks } = useContext(AuthContext);
+  const { artworks, setArtworks } = useContext(AuthContext)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [alertMessage, setAlertMessage] = useState(null);
+  const [snackType, setSnackType] = useState('');
+
+  const handleDelete = (artworkId) => {
+    const deleteEndpoint = `/artworks/${artworkId}`;
+  
+    fetch(deleteEndpoint, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          setArtworks((prevArtworks) =>
+            prevArtworks.filter((artwork) => artwork.id !== artworkId)
+          );
+          setSnackType('success');
+          setAlertMessage('Artwork deleted successfully');
+        } else {
+          setAlertMessage('Error deleting artwork');
+        }
+      })
+      .catch((error) => {
+        setSnackType('error');
+        setAlertMessage('Error deleting artwork');
+      })
+      .finally(() => {
+        setIsDeleting(true);
+      });
+  };
 
   return (
     <>
       <MenuBar />
       <br />
-      <Gallery artworks={artworks}/>
+      <Gallery artworks={artworks} onDelete={handleDelete} />
+      <AlertBar
+          message={alertMessage}
+          setAlertMessage={setAlertMessage}
+          snackType={snackType}
+          handleSnackType={setSnackType}
+        />
     </>
   )
 }
